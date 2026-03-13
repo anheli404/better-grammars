b07efbfb2b3582e16b955f5895a16af06fb37e85
anheli404 <anhelinakichihina@gmail.com>
2026-03-13 20:56:08 +0100

    Tiny LittleConfig changes

--- src/compression/LocalConfig.java
+++ src/compression/LocalConfig.java
@@ -9,8 +9,6 @@ public class LocalConfig {
 
 	// TODO Change this to your local path of the git repository clone
 	public static final String GIT_ROOT = ".";
-	public static final ArithmeticCodingFactory.Backend AC_BACKEND =
-			ArithmeticCodingFactory.Backend.BIG_DECIMAL;
-
+	public static final ArithmeticCodingFactory.Backend AC_BACKEND = ArithmeticCodingFactory.Backend.NAYUKI;
 
 }
